5c7621a0c1b7155d94aff97a857f347fdef27b09
bilger-progress <bilger.yahov@progress.com>
2018-11-28 14:08:03 +0200

    Initialize the Android project.

---
+++ app/src/main/java/com/pigster/kinvey_team/pigster_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pigster.kinvey_team.pigster_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
